b915681a069d89fd863585210aff86579956470f
adminarzfivecoin <five.coin.crypto1404@gmail.com>
2025-08-25 02:37:31 +0330

    Create walletconnector.tsx

---
+++ walletconnector.tsx
@@ -0,0 +1,9 @@
+import { TonConnectButton } from '@tonconnect/ui-react';
+
+export default function WalletConnector() {
+  return (
+    <div className="flex justify-end">
+      <TonConnectButton />
+    </div>
+  );
+}
